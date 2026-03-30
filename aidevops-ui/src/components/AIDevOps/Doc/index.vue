56c9867ecf2d5fab4f78c190d65b3f9592671d65
Matirx <1324434947@qq.com>
2026-03-30 03:53:30 +0000

    docs: clean deployment paths and doc links

---
+++ aidevops-ui/src/components/AIDevOps/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AIDevOpsDoc',
+  data() {
+    return {
+      url: 'http://doc.devops.zoudekang.cloud'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
