4489b2ae5ab0dba38f29b10ef3ffca9ca83377d3
Matirx <1324434947@qq.com>
2026-03-30 03:59:37 +0000

    refactor: update script paths to aidevops modules

--- bin/run-auth.bat
+++ bin/run-auth.bat
@@ -1,14 +1,14 @@
 @echo off
 echo.
-echo [信息] 使用Jar命令运行Auth工程。
+echo [息] 使JarAuth獭
 echo.
 
 cd %~dp0
-cd ../ruoyi-auth/target
+cd ../aidevops-auth/target
 
 set JAVA_OPTS=-Xms512m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 
-java -Dfile.encoding=utf-8 %JAVA_OPTS% -jar ruoyi-auth.jar
+java -Dfile.encoding=utf-8 %JAVA_OPTS% -jar aidevops-auth.jar
 
 cd bin
 pause
