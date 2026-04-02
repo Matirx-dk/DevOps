b37271eef6da85f28b2e2d8e14a53b1f3b033533
Matirx <1324434947@qq.com>
2026-04-02 10:43:02 +0000

    feat(ui): polish profile avatar, notices, and sidebar branding

--- aidevops-ui/src/layout/components/Sidebar/Logo.vue
+++ aidevops-ui/src/layout/components/Sidebar/Logo.vue
@@ -2,70 +2,77 @@
   <div class="sidebar-logo-container" :class="{'collapse':collapse}" :style="{ backgroundColor: sideTheme === 'theme-dark' && navType !== 3 ? variables.menuBackground : variables.menuLightBackground }">
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo" />
-        <h1 v-else class="sidebar-title" :style="{ color: sideTheme === 'theme-dark' && navType !== 3 ? variables.logoTitleColor : variables.logoLightTitleColor }">{{ title }} </h1>
+        <div class="brand-badge" v-if="logoText">{{ logoText }}</div>
+        <img v-else-if="logo" :src="logo" class="sidebar-logo" />
+        <h1 v-else class="sidebar-title" :style="{ color: sideTheme === 'theme-dark' && navType !== 3 ? variables.logoTitleColor : variables.logoLightTitleColor }">{{ title }}</h1>
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo" />
-        <h1 class="sidebar-title" :style="{ color: sideTheme === 'theme-dark' && navType !== 3 ? variables.logoTitleColor : variables.logoLightTitleColor }">{{ title }} </h1>
+        <div class="brand-badge" v-if="logoText">{{ logoText }}</div>
+        <img v-else-if="logo" :src="logo" class="sidebar-logo" />
+        <h1 class="sidebar-title" :style="{ color: sideTheme === 'theme-dark' && navType !== 3 ? variables.logoTitleColor : variables.logoLightTitleColor }">{{ title }}</h1>
       </router-link>
     </transition>
   </div>
 </template>
 
 <script>
-import logoImg from '@/assets/logo/logo.svg'
 import variables from '@/assets/styles/variables.scss'
 
 export default {
   name: 'SidebarLogo',
   props: {
-    collapse: {
-      type: Boolean,
-      required: true
-    }
+    collapse: { type: Boolean, required: true }
   },
   computed: {
-    variables() {
-      return variables
-    },
-    sideTheme() {
-      return this.$store.state.settings.sideTheme
-    },
-    navType() {
-      return this.$store.state.settings.navType
-    }
+    variables() { return variables },
+    sideTheme() { return this.$store.state.settings.sideTheme },
+    navType() { return this.$store.state.settings.navType }
   },
   data() {
     return {
       title: process.env.VUE_APP_TITLE,
-      logo: logoImg
+      logo: '',
+      logoText: 'A'
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.sidebarLogoFade-enter-active {
-  transition: opacity 1.5s;
-}
-
+.sidebarLogoFade-enter-active { transition: opacity 1.5s; }
 .sidebarLogoFade-enter,
-.sidebarLogoFade-leave-to {
-  opacity: 0;
-}
+.sidebarLogoFade-leave-to { opacity: 0; }
 
 .sidebar-logo-container {
   position: relative;
-  height: 50px;
-  line-height: 50px;
-  background: #2b2f3a;
+  height: 56px;
+  line-height: 56px;
+  background: transparent;
   text-align: center;
   overflow: hidden;
 
   & .sidebar-logo-link {
     height: 100%;
     width: 100%;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+
+    & .brand-badge {
+      width: 34px;
+      height: 34px;
+      border-radius: 50%;
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      color: #fff;
+      font-size: 18px;
+      font-weight: 700;
+      background: linear-gradient(135deg, #3fa9ff 0%, #215cff 100%);
+      box-shadow: 0 10px 24px rgba(33, 92, 255, 0.28);
+      flex-shrink: 0;
+    }
 
     & .sidebar-logo {
       width: 32px;
@@ -78,18 +85,19 @@ export default {
       display: inline-block;
       margin: 0;
       color: #fff;
-      font-weight: 600;
-      line-height: 50px;
+      font-weight: 700;
+      line-height: 56px;
       font-size: 14px;
+      letter-spacing: 0.2px;
       font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
       vertical-align: middle;
     }
   }
 
   &.collapse {
-    .sidebar-logo {
-      margin-right: 0px;
-    }
+    .brand-badge,
+    .sidebar-logo { margin-right: 0; }
+    .sidebar-logo-link { gap: 0; }
   }
 }
 </style>
--- aidevops-ui/src/views/system/user/profile/userAvatar.vue
+++ aidevops-ui/src/views/system/user/profile/userAvatar.vue
@@ -1,31 +1,38 @@
 <template>
   <div>
-    <div class="user-info-head" @click="editCropper()"><img v-bind:src="options.img" title="点击上传头像" class="img-circle img-lg" /></div>
-    <el-dialog :title="title" :visible.sync="open" width="800px" append-to-body @opened="modalOpened"  @close="closeDialog">
-      <el-row>
+    <div class="user-info-head" @click="editCropper()">
+      <img v-bind:src="options.img" title="点击上传头像" class="img-circle img-lg" />
+      <div class="avatar-edit-tip">更换头像</div>
+    </div>
+    <el-dialog :title="title" :visible.sync="open" width="800px" append-to-body @opened="modalOpened" @close="closeDialog">
+      <el-row class="avatar-editor-row">
         <el-col :xs="24" :md="12" :style="{height: '350px'}">
-          <vue-cropper
-            ref="cropper"
-            :img="options.img"
-            :info="true"
-            :autoCrop="options.autoCrop"
-            :autoCropWidth="options.autoCropWidth"
-            :autoCropHeight="options.autoCropHeight"
-            :fixedBox="options.fixedBox"
-            :outputType="options.outputType"
-            @realTime="realTime"
-            v-if="visible"
-          />
+          <div class="cropper-pane">
+            <vue-cropper
+              ref="cropper"
+              :img="options.img"
+              :info="true"
+              :autoCrop="options.autoCrop"
+              :autoCropWidth="options.autoCropWidth"
+              :autoCropHeight="options.autoCropHeight"
+              :fixedBox="options.fixedBox"
+              :outputType="options.outputType"
+              @realTime="realTime"
+              v-if="visible"
+            />
+          </div>
         </el-col>
         <el-col :xs="24" :md="12" :style="{height: '350px'}">
           <div class="avatar-upload-preview">
-            <img :src="previews.url" :style="previews.img" />
+            <div class="preview-frame">
+              <img :src="previews.url" :style="previews.img" />
+            </div>
           </div>
         </el-col>
       </el-row>
       <br />
-      <el-row>
-        <el-col :lg="2" :sm="3" :xs="3">
+      <el-row class="avatar-toolbar">
+        <el-col :lg="3" :sm="4" :xs="5">
           <el-upload action="#" :http-request="requestUpload" :show-file-list="false" :before-upload="beforeUpload">
             <el-button size="small">
               选择
@@ -33,19 +40,11 @@
             </el-button>
           </el-upload>
         </el-col>
-        <el-col :lg="{span: 1, offset: 2}" :sm="2" :xs="2">
-          <el-button icon="el-icon-plus" size="small" @click="changeScale(1)"></el-button>
-        </el-col>
-        <el-col :lg="{span: 1, offset: 1}" :sm="2" :xs="2">
-          <el-button icon="el-icon-minus" size="small" @click="changeScale(-1)"></el-button>
-        </el-col>
-        <el-col :lg="{span: 1, offset: 1}" :sm="2" :xs="2">
-          <el-button icon="el-icon-refresh-left" size="small" @click="rotateLeft()"></el-button>
-        </el-col>
-        <el-col :lg="{span: 1, offset: 1}" :sm="2" :xs="2">
-          <el-button icon="el-icon-refresh-right" size="small" @click="rotateRight()"></el-button>
-        </el-col>
-        <el-col :lg="{span: 2, offset: 6}" :sm="2" :xs="2">
+        <el-col :lg="1" :sm="2" :xs="3"><el-button icon="el-icon-plus" size="small" @click="changeScale(1)"></el-button></el-col>
+        <el-col :lg="1" :sm="2" :xs="3"><el-button icon="el-icon-minus" size="small" @click="changeScale(-1)"></el-button></el-col>
+        <el-col :lg="1" :sm="2" :xs="3"><el-button icon="el-icon-refresh-left" size="small" @click="rotateLeft()"></el-button></el-col>
+        <el-col :lg="1" :sm="2" :xs="3"><el-button icon="el-icon-refresh-right" size="small" @click="rotateRight()"></el-button></el-col>
+        <el-col :lg="3" :sm="4" :xs="5" :offset="6">
           <el-button type="primary" size="small" @click="uploadImg()">提 交</el-button>
         </el-col>
       </el-row>
@@ -63,61 +62,39 @@ export default {
   components: { VueCropper },
   data() {
     return {
-      // 是否显示弹出层
       open: false,
-      // 是否显示cropper
       visible: false,
-      // 弹出层标题
       title: "修改头像",
       options: {
-        img: store.getters.avatar,  //裁剪图片的地址
-        autoCrop: true,             // 是否默认生成截图框
-        autoCropWidth: 200,         // 默认生成截图框宽度
-        autoCropHeight: 200,        // 默认生成截图框高度
-        fixedBox: true,             // 固定截图框大小 不允许改变
-        outputType:"png",           // 默认生成截图为PNG格式
-        filename: 'avatar'          // 文件名称
+        img: store.getters.avatar,
+        autoCrop: true,
+        autoCropWidth: 200,
+        autoCropHeight: 200,
+        fixedBox: true,
+        outputType:"png",
+        filename: 'avatar'
       },
       previews: {},
       resizeHandler: null
     }
   },
   methods: {
-    // 编辑头像
-    editCropper() {
-      this.open = true
-    },
-    // 打开弹出层结束时的回调
+    editCropper() { this.open = true },
     modalOpened() {
       this.visible = true
       if (!this.resizeHandler) {
-        this.resizeHandler = debounce(() => {
-          this.refresh()
-        }, 100)
+        this.resizeHandler = debounce(() => { this.refresh() }, 100)
       }
       window.addEventListener("resize", this.resizeHandler)
     },
-    // 刷新组件
-    refresh() {
-      this.$refs.cropper.refresh()
-    },
-    // 覆盖默认的上传行为
-    requestUpload() {
-    },
-    // 向左旋转
-    rotateLeft() {
-      this.$refs.cropper.rotateLeft()
-    },
-    // 向右旋转
-    rotateRight() {
-      this.$refs.cropper.rotateRight()
-    },
-    // 图片缩放
+    refresh() { this.$refs.cropper.refresh() },
+    requestUpload() {},
+    rotateLeft() { this.$refs.cropper.rotateLeft() },
+    rotateRight() { this.$refs.cropper.rotateRight() },
     changeScale(num) {
       num = num || 1
       this.$refs.cropper.changeScale(num)
     },
-    // 上传预处理
     beforeUpload(file) {
       if (file.type.indexOf("image/") == -1) {
         this.$modal.msgError("文件格式错误，请上传图片类型,如：JPG，PNG后缀的文件。")
@@ -130,7 +107,6 @@ export default {
         }
       }
     },
-    // 上传图片
     uploadImg() {
       this.$refs.cropper.getCropBlob(data => {
         let formData = new FormData()
@@ -144,11 +120,7 @@ export default {
         })
       })
     },
-    // 实时预览
-    realTime(data) {
-      this.previews = data
-    },
-    // 关闭窗口
+    realTime(data) { this.previews = data },
     closeDialog() {
       this.options.img = store.getters.avatar
       this.visible = false
@@ -160,25 +132,74 @@ export default {
 <style scoped lang="scss">
 .user-info-head {
   position: relative;
-  display: inline-block;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 120px;
   height: 120px;
+  border-radius: 50%;
+  overflow: hidden;
+  background: linear-gradient(135deg, #101a2d 0%, #1c2a45 100%);
+  box-shadow: 0 14px 32px rgba(15, 23, 42, 0.2);
+}
+
+.user-info-head img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.avatar-edit-tip {
+  position: absolute;
+  left: 50%;
+  bottom: 10px;
+  transform: translateX(-50%);
+  padding: 4px 10px;
+  border-radius: 999px;
+  background: rgba(15, 23, 42, 0.68);
+  color: #eaf2ff;
+  font-size: 12px;
+  opacity: 0;
+  transition: opacity .2s ease;
+  pointer-events: none;
 }
 
 .user-info-head:hover:after {
-  content: '+';
+  content: '';
   position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  color: #eee;
-  background: rgba(0, 0, 0, 0.5);
-  font-size: 24px;
-  font-style: normal;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.28);
   cursor: pointer;
-  line-height: 110px;
-  border-radius: 50%;
+}
+.user-info-head:hover .avatar-edit-tip { opacity: 1; }
+
+.avatar-editor-row { margin-top: 6px; }
+.cropper-pane,
+.preview-frame {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 18px;
+  background: rgba(255,255,255,0.04);
+  border: 1px solid rgba(255,255,255,0.08);
+  overflow: hidden;
+}
+
+.avatar-upload-preview {
+  width: 100%;
+  height: 100%;
+}
+
+.preview-frame img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+  border-radius: 16px;
+}
+
+.avatar-toolbar {
+  display: flex;
+  align-items: center;
 }
 </style>
