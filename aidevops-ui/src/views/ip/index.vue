64fbf42f3fbf9edea45b037bdf33a5abb86c8be4
Matirx <matirx@example.com>
2026-05-14 07:48:36 +0000

    temp: minimal empty IP component

--- aidevops-ui/src/views/ip/index.vue
+++ aidevops-ui/src/views/ip/index.vue
@@ -1,25 +1,9 @@
-<!--
 <template>
-  <div class="app-container page-shell page-ip">
-    IP管理页面（已注释）
-  </div>
+  <div></div>
 </template>
 
 <script>
-// const STORAGE_KEY = '__ip_data__'
-// const DEFAULT_DATA = [...]
-// function getStoredData() { ... }
-// function saveData(data) { ... }
-// function formatDate() { ... }
-// export default {
-//   name: 'IpManagement',
-//   data() { ... },
-//   created() { this.getList() },
-//   methods: { ... }
-// }
+export default {
+  name: 'IpManagement'
+}
 </script>
-
-<style lang="scss" scoped>
-// .page-shell { ... }
-</style>
--->
